6192a06f468d55979b86d8c30130f5256c1b4e28
mku777 <mkm.kuz777@gmail.com>
2022-09-29 13:40:37 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,36 @@
+// Напиши скрипт который:
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const navItemsEl = document.querySelectorAll(".item")
+console.log (`Number of categories: ${navItemsEl.length}`)
+
+const allTittleEl = document.querySelectorAll("h2")
+
+console.log('')
+const animalsEl = navItemsEl[0].querySelectorAll("li")
+console.log(`Category : ${allTittleEl[0].textContent}`)
+console.log(`Elements : ${animalsEl.length}`);
+
+console.log('')
+const productsEl = navItemsEl[1].querySelectorAll("li")
+console.log(`Category : ${allTittleEl[1].textContent}`)
+console.log(`Elements : ${productsEl.length}`);
+
+console.log('')
+const technologiesEl = navItemsEl[2].querySelectorAll("li")
+console.log(`Category : ${allTittleEl[2].textContent}`)
+console.log(`Elements : ${technologiesEl.length}`);
