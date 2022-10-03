855e1c5189cc5053ff9ce70e263543e8c783152d
mku777 <mkm.kuz777@gmail.com>
2022-10-03 10:54:04 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -18,3 +18,28 @@
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value !== "" &&
+    event.currentTarget.elements.password.value !== ""
+  ) {
+    const dataForm = {
+      [event.currentTarget.elements.email.name]:
+        event.currentTarget.elements.email.value,
+      [event.currentTarget.elements.password.name]:
+        event.currentTarget.elements.password.value,
+    };
+    console.log(dataForm);
+    event.currentTarget.reset();
+    return;
+  }
+  alert("Всі поля мають бути заповнені.");
+}
+
